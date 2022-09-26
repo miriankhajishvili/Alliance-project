64225889a17b84f77476064daf21fd1aa4134ede
mirian.khajishvili@gmail.com <mirian.khajishvili@gmail.com>
2022-09-27 00:19:08 +0400

    added validation to form

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const firstname = document.getElementById('firstname')
 const lastname = document.getElementById('lastname')
+const address = document.getElementById('address')
+const small = document.getElementById('small')
 
 
 
@@ -14,22 +16,43 @@ function showError(input, massage){
 }
 function showSuccess(input){
     const formControl = input.parentElement;
-    formControl.className= "text-success";
+    formControl.className= "text-success ";
+    const small = formControl.querySelector('small');
+    small.className = "invisible"
 }
     
 function checkRequared(inputArr){
     inputArr.forEach(function(input){
        if (input.value.trim()=== '') {
-     showError(input, `${input.value} is requared`);
+     showError(input, `${getFieldname(input)} is requared`);
        } else {
         showSuccess(input);
        }
     });}
 
+
+
+    function checkLength(input, min, max){
+        if(input.value.length < min) {
+            showError(input, `${getFieldname(input)} must be at least ${min} charaters`);
+        } else if (input.value.length > max) {
+            showError(input, `${getFieldname(input)} must be less than  ${max} charaters`);
+        } else {
+            showSuccess(input);
+        }
+    }
+    
+
     form.addEventListener('submit', function(e)  {
         e.preventDefault();
     
-       checkRequared([firstname, lastname ]);
+       checkRequared([firstname, lastname, address ]);
+       checkLength(address,3, 35);
       
 
     });
+
+    function getFieldname(input){
+        return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    ;}
+
